91c1056d620d01195eb46727b1862acad92526f7
Fer Torres <fertorresmx@gmail.com>
2020-06-16 22:38:18 -0500

    :sparkles: Create new methods

--- agenda/contacts.py
+++ agenda/contacts.py
@@ -3,62 +3,97 @@
 
 class Contact:
 
-    def __init__(self, name, phone, email):
-        self._name = name
-        self._phone = phone
-        self._email = email
-
+  def __init__(self, name, phone, email):
+    self.name = name
+    self.phone = phone
+    self.email = email
 
 class ContactBook:
 
-    def __init__(self):
-        self._contacts = []
+  def __init__(self):
+    self._contacts = []
 
-    def add(self, name, phone, email):
-        print('name: {}, phone: {}, email: {}'.format(name, phone, email))
+  def add(self, name, phone, email):
+    contact = Contact(name, phone, email)
+    self._contacts.append(contact)
+    print('name: {}, phone: {}, email: {}'.format(name, phone, email))
 
+  def show_all(self):
+    for contact in self._contacts:
+      self._print_contact(contact)
+
+  def delete(self, name):
+    for idx, contact in enumerate(self._contacts):
+      if contact.name.lower() == name.lower():
+        del self._contacts[idx]
+        break
+    else:
+      self._not_found()
+
+  def _print_contact(self, contact):
+    print('--- * --- * --- * --- * --- * --- * --- * --- * ---')
+    print('Nombre: {}'.format(contact.name))
+    print('Teléfono: {}'.format(contact.phone))
+    print('Email: {}'.format(contact.email))
+    print('--- * --- * --- * --- * --- * --- * --- * --- * ---')
+
+  def search(self, name):
+    for contact in self._contacts:
+      if contact.name.lower() == name.lower():
+        self._print_contact(contact)
+        break
+    else:
+      self._not_found()
+
+  def _not_found(self):
+    print('***')
+    print('No encontrado!')
+    print('***')
 
 def run():
 
-    contact_book = ContactBook()
+  contact_book = ContactBook()
 
-    while True:
-        command = str(input('''
-            ¿Qué deseas hacer?
+  while True:
+    command = str(input('''
+      ¿Qué deseas hacer?
 
-            [a]ñadir contacto
-            [ac]tualizar contacto
-            [b]uscar contacto
-            [e]liminar contacto
-            [l]istar contactos
-            [s]alir
-        '''))
+      [a]ñadir contacto
+      [ac]tualizar contacto
+      [b]uscar contacto
+      [e]liminar contacto
+      [l]istar contactos
+      [s]alir
+    '''))
 
-        if command == 'a':
-            name = str(input('Escribe el nombre del contacto: '))
-            phone = str(input('Escribe el tel del contacto: '))
-            email = str(input('Escribe el email del contacto: '))
+    if command == 'a':
+      name = str(input('Escribe el nombre del contacto: '))
+      phone = str(input('Escribe el tel del contacto: '))
+      email = str(input('Escribe el email del contacto: '))
 
-            contact_book.add(name, phone, email)
+      contact_book.add(name, phone, email)
 
-        elif command == 'ac':
-            print('actualizar contacto')
+    elif command == 'ac':
+      print('actualizar contacto')
 
-        elif command == 'b':
-            print('buscar contacto')
+    elif command == 'b':
+      name = str(input('Escribe el nombre del contacto que buscas: '))
+      contact_book.search(name)
 
-        elif command == 'e':
-            print('eliminar contacto')
+    elif command == 'e':
+      name = str(input('Escribe el nombre del contacto a borrar: '))
+      contact_book.delete(name)
 
-        elif command == 'l':
-            print('listar contactos')
+    elif command == 'l':
+      print('listar contactos')
+      contact_book.show_all()
 
-        elif command == 's':
-            break
-        else:
-            print('Comando no encontrado.')
+    elif command == 's':
+      break
+    else:
+      print('Comando no encontrado.')
 
 
 if __name__ == '__main__':
-    print('B I E N V E N I D O  A  L A  A G E N D A')
-    run()
+  print('B I E N V E N I D O  A  L A  A G E N D A')
+  run()
